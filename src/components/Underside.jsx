ea0f7153e969795cc6d17c555177bb21441d7165
sharshekeev13 <dastan.sharshekeevv@gmail.com>
2024-06-27 14:47:16 +0600

    edit underscore

--- src/components/Underside.jsx
+++ src/components/Underside.jsx
@@ -33,78 +33,78 @@ const Underside = () => {
           <img
             className="snk"
             width="250px"
-            src={t("underside.images.snk_logo") || snk_logo}
+            src={snk_logo}
             alt="SNK Logo"
           />
           <img
             className="ph1"
             width="200px"
-            src={t("underside.images.photo1") || photo1}
+            src={photo1}
             alt="The King of Fighters"
           />
           <img
             className="ph2"
             width="200px"
-            src={t("underside.images.photo2") || photo2}
+            src={photo2}
             alt="Fatal Fury"
           />
           <img
             className="ph3"
             width="180px"
-            src={t("underside.images.photo3") || photo3}
+            src={photo3}
             alt="Art of Fighting"
           />
           <img
             className="ph4"
             width="180px"
-            src={t("underside.images.photo4") || photo4}
+            src={photo4}
             alt="Metal Slug"
           />
           <img
             className="ph5"
             width="200px"
-            src={t("underside.images.photo5") || photo5}
+            src={photo5}
             alt="Athena"
           />
         </div>
         <div className="logo-section-center">
-          <img width="140px" src={t("underside.images.x") || x} alt="" />
+          <img width="140px" src={x} alt="" />
         </div>
         <div className="logo-section-right">
           <img
             width="250px"
             className="capcom"
-            src={t("underside.images.capcom_logo") || capcom_logo}
+            src={capcom_logo}
             alt="Capcom Logo"
           />
           <img
             width="200px"
             className="photo1"
-            src={t("underside.images.capcom_photo1") || photo1}
+            src={photo1}
             alt="Street Fighter"
           />
           <img
             width="240px"
             className="photo2"
-            src={t("underside.images.capcom_photo2") || photo2}
+            src={photo2}
             alt=""
           />
           <img
             width="200px"
             className="photo3"
-            src={t("underside.images.capcom_photo3") || photo3}
+            src={photo3}
             alt=""
           />
           <img
             width="200px"
             className="photo4"
-            src={t("underside.images.capcom_photo4") || photo4}
+            src={photo4}
             alt=""
           />
           <img
             width="200px"
             className="photo5"
-            src={t("underside.images.capcom_photo5") || photo5}
+            src={photo5}
             alt=""
           />
         </div>
